cbf6f3b647a174dc726b348e82cde6b0e236825b
Rangerbearr <eli.orille@yahoo.com>
2024-09-04 08:52:36 -0500

    Set up project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('hi');
+const projectList = [];
+
+function project(title,description,dueDate,priority){
+    return{
+        todos: [],
+        title: title,
+        description: description,
+        dueDate: dueDate,
+        priority: priority
+    }
+
+    // project.title = title;
+    // project.description = description;
+    // project.dueDate = dueDate;
+    // project.priority = priority;
+
+}
+
+function todoItem(title,description,dueDate,priority){
+
+}
+
+const projectSubmit = document.getElementById('submitForm');
+
+projectSubmit.addEventListener('click',()=>{
+    const title = document.getElementById('projectName').value;
+    const description = document.getElementById('projectDescription').value;
+    const dueDate = document.getElementById('dueDate').value;
+    const pj = project(title,description,dueDate,null);
+    console.log(pj.title);
+});
+

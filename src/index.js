d59d5d6babad6acdd72442b03804ab62726f65d7
Rangerbearr <eli.orille@yahoo.com>
2024-09-07 16:03:49 -0500

    Made it so todos end up under correct project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 const projectList = [];
+let todoProjectNumber = 0;
 
 function project(title,description,dueDate,priority){
     return{
@@ -12,9 +13,12 @@ function project(title,description,dueDate,priority){
 }
 
 function createProject(project){
+    projectList.push(project);
     const projectHolder = document.getElementById('projectHolder');
     const projectBox = document.createElement('div');
     projectBox.classList.add('projectBlock');
+    //index to help identify block when adding todos.
+    projectBox.setAttribute('data-index-number', projectList.indexOf(project));
     const title = document.createElement('h1');
     title.textContent = project.title;
     const description = document.createElement('h5');
@@ -23,15 +27,46 @@ function createProject(project){
     const todoItemCreator = document.createElement('button');
     todoItemCreator.id = 'createTodoButton';
     todoItemCreator.textContent = 'Add Todo'
-    todoButtonFunction(todoItemCreator);
+    // todoButtonFunction(todoItemCreator);
+
+    todoItemCreator.addEventListener('click',()=>{
+        const block = document.getElementById('todoSubmitBlock');
+        // makes the todo appear under the corresponding project.
+        todoProjectNumber = projectBox.dataset.indexNumber;
+        block.setAttribute('style','display: block');
+    });
 
     projectBox.appendChild(title);
     projectBox.appendChild(description);
     projectBox.appendChild(todoItemCreator);
-
     projectHolder.appendChild(projectBox);
 
 }
+
+// creating function for when todo submit button is pressed.
+const todoSubmit = document.getElementById('todoSubmitForm');
+
+todoSubmit.addEventListener('click',()=>{
+    const todoTitle = document.getElementById('todoName').value;
+    const todoDescription = document.getElementById('todoDescription').value;
+    const todoPriority = document.getElementById('todoPriority').value;
+    // const todoProjectNumber = projectBox.dataset.indexNumber;
+    // fill todo with user input
+    const todo = todoItem(todoTitle,todoDescription,todoPriority,todoProjectNumber);
+    // add todo item into todo array in project
+    projectList[todoProjectNumber].todos.push(todo);
+    // remove each todoBox to avoid duplicates
+
+    // use css selector to only delete specific things
+    const alltodos = document.querySelectorAll(`[data-index-number="${todoProjectNumber}"] > .todoBox`);
+    console.log(alltodos);
+    alltodos.forEach(box => {
+        box.parentElement.removeChild(box);
+    });
+    // create todo in DOM
+    displayTodos(todoProjectNumber);
+    todoSubmit.parentElement.setAttribute('style','display: none');
+});
 
 const projectSubmit = document.getElementById('submitForm');
 projectSubmit.addEventListener('click',()=>{
@@ -53,37 +88,30 @@ projectCreateButton.addEventListener('click',()=>{
     block.setAttribute('style','display: block');
 });
 
-function createTodo(todoItem){
+function todoItem(title,description,priority,projectNumber){
     return{
         title: title,
         description: description,
-        priority: priority
+        priority: priority,
+        projectNumber: projectNumber
     }
 }
 
-function todoItem(title,description,priority){
-    return{
-        title: title,
-        description: description,
-        priority: priority
+function displayTodos(index){
+    // console.log(index);
+    const projectHolder = document.querySelector(`[data-index-number="${index}"]`);
+    const projectTodos = projectList[index].todos;
+    for(let i = 0; i<projectTodos.length; i++){
+        const todoBox = document.createElement('div');
+        todoBox.classList.add('todoBox');
+        const title = document.createElement('p');
+        title.textContent = projectTodos[i].title;
+        const description = document.createElement('em');
+        description.textContent = projectTodos[i].description;
+
+        todoBox.appendChild(title);
+        todoBox.appendChild(description);
+
+        projectHolder.appendChild(todoBox);
     }
 }
-
-function todoButtonFunction(button){
-    button.addEventListener('click',()=>{
-        const block = document.getElementById('todoSubmitBlock');
-        block.setAttribute('style','display: block');
-    });
-}
-
-const todoSubmit = document.getElementById('createTodoButton');
-todoSubmit.addEventListener('click',()=>{
-    const title = document.getElementById('todoName').value;
-    const description = document.getElementById('todoDescription').value;
-    const priority = document.getElementById('todoPriority').value;
-    // fill todo with user input
-    const todo = todoItem(title,description,priority);
-    console.log(todo.title);
-    // create todo in DOM
-    createTodo(todo);
-});
